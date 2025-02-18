e67c482c48cce09cd80e13a71809b25b7f996d09
M.P Usama <136558201+Usamafuward@users.noreply.github.com>
2025-02-18 13:39:41 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://backend:8001"; // FastAPI backend URL
+const baseUrl = "http://localhost:8001"; // FastAPI backend URL
 let isWebRTCActive = false;
 let peerConnection;
 let dataChannel;
