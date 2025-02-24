383b8d16306b527b9b1a8c34f37ce694a4d3a756
Umair Fuward <usamafuward2001@gmail.com>
2025-02-24 13:38:59 +0530

    Reapply "Save current changes"
    This reverts commit 3fbb5be275b83cda7613064c422a6798754488df.

--- ocr_back/main.py
+++ ocr_back/main.py
@@ -16,15 +16,10 @@ load_dotenv()
 
 app = FastAPI()
 
-origins = [
-    "https://poc-cv-chat.kainovation.com",
-    "http://localhost:8000",
-]
-
 # CORS middleware to allow frontend to communicate with backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
